03d624340d8615b1046a4c9796de30e56ec969ac
Joshua Nwankwo <systemblakes@gmail.com>
2019-10-28 18:14:40 +0100

    restructured unresolved files

--- src/app/App.js
+++ src/app/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 
 
-import FindADoctorNav from '../components/find-a-doctor-Nav/find-a-doctor-nav';
-import LandingPage from './Landing-page/Landing-Page';
+import FindADoctorNav from '../components/findADoctorNav/navBar';
+import LandingPage from '../components/Landing-page/Landing-Page';
 import LoginPage from '../components/LoginPage/loginpage';
 import SignUp from '../components/SignupPage/signup';
 import Navbar from '../components/navbar/navbar';
-import DoctorsProfile from './DoctorsProfile/DoctorsProfile'
-import HomePage from '../components/homePage/HomePage'
-import Symptoms from '../components/Symptoms/Symptoms'
-import BookDoc from '../components/bookDoc/BookDoc'
+import DoctorsProfile from '../components/DoctorsProfile/DoctorsProfile';
+import HomePage from '../components/homePage/HomePage';
+import Symptoms from '../components/Symptoms/Symptoms';
+import BookDoc from '../components/bookDoc/BookDoc';
 import FindADoctor from '../components/find-a-doc/find-a-doctor';
 import SelectSearchedDoctor from '../components/selectSearchDoctors/select-Search-Doctor';
 
@@ -18,7 +18,7 @@ import SelectSearchedDoctor from '../components/selectSearchDoctors/select-Searc
   
 class App extends React.Component{
     render(){
-
+        <LandingPage />
     }
 }
 
